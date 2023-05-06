01509e13b989332edb3b2ae29392e5e2c7a34d66
zakede <crazygamingmannat1998@gmail.com>
2023-05-06 02:52:30 -0400

    Add More Functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let color = "black";
+let click = true;
 
 function populateBoard(size) {
 
@@ -32,12 +33,31 @@ function changeSize(input) {
 
 
 function colorSquare() {
-    this.style.backgroundColor = color;
+    if (click){
+        if (color === "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }else {
+            this.style.backgroundColor = color;
+        }
+    }
 }
 
 function changeColor(choice) {
     color = choice;
 }
-  
 
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "whitesmoke");
+}
+
+document.querySelector("body").addEventListener("click", () => {
+    click = !click
+    if(click) {
+        document.querySelector(".mode").textContent = "Mode: Coloring!";
+    }else{
+        document.querySelector(".mode").textContent = "Mode: Not Coloring!";
+    }
+});
 document.getElementById("boxError").addEventListener("click");
